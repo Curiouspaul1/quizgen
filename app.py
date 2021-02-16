784874f1ae669861cda8895e9544a5bffdab90a3
Curiouspaul1 <170408025@live.unilag.edu.ng>
2021-02-16 19:45:28 +0100

    added procfile

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, session
+from flask import Flask, render_template, url_for, request
 from faunadb import query as q
 from faunadb.client import FaunaClient
 from dotenv import load_dotenv
